1d95666c5f074d2d8258a701c718457e0038a88c
Lucca Trevisan <141439341+luccatrevisan@users.noreply.github.com>
2026-04-30 14:21:23 -0300

    beautiful soup logic and data extraction in json

--- scraper.py
+++ scraper.py
@@ -1,8 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
+# this code get the brand's menu and return all items with their respective prices
 r = requests.get("https://chewiecookies.com.br")
 html = r.text
 
 soup = BeautifulSoup(html, "html.parser")
+
+
+items = []
+for item in soup.find_all(class_ = "item-produto"):
+    '''get the name and the price'''
+    name = item.h3.text
+    price = item.strong.text
+
+    items.append({
+        "name" : name.strip(),
+        "price" : price.strip()
+    })
+
+print(items)
+
+# with open("items.json", "w", encoding="utf-8") as file:
+#    json.dump(items, file, ensure_ascii=False, indent=2)
